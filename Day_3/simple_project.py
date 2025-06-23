ca1794d7f2e63d02cf40b626e116072e503b880a
Hanzla-Nawaz <hanzlanawaz@outlook.com>
2025-06-23 18:35:51 +0500

    write login method

--- Day_3/simple_project.py
+++ Day_3/simple_project.py
@@ -7,16 +7,38 @@ class chatbook():
 
     def menu(self):
         user_input = input("""Welcome to ChatBook!
-Please choose an option:
-1. Create Account
-2. Login    
-3. Exit
-Enter your choice: """)
+        Please choose an option:
+        1. Create Account
+        2. Login    
+        3. Exit
+        Enter your choice: """)
         if user_input == "1":
-                pass
+            self.create_account()
         elif user_input == "2":
-            pass
+            self.login()
         elif user_input == "3":
             print("Exiting ChatBook. Goodbye!")
             exit()
 
+
+
+
+    def create_account(self):
+        self.username = input("Enter your username:")
+        self.password = input("Enter your password:")
+        self.email = input("Enter your email: ")
+        print(f"Account created successfully for {self.username}!")
+        self.menu()
+
+
+    def login(self):
+        username = input("Enter your username:")
+        password = input("Enter your password:")
+        if username == self.username and password == self.password:
+            print(f"Welcome back, {self.username}!")
+        else:print("Invalid username or password. Please try again. ")
+        self.menu()
+
+
+
+obj = chatbook()  
